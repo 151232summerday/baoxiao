b1cbab819c1b7a1ec5ab9cddad20467b1ed3afeb
Your Name <liuhao151232@126.com>
2019-10-10 23:26:40 +0800

    修改初始化，保证初始解可行

--- baoxaio.m
+++ baoxaio.m
@@ -3,8 +3,8 @@ clear;
 tic;
 
 %% The initial parameters
-amount_baoxiao = 62;%要报金额
-invoice = [8,10,11,10,10,17,10,10,16,11,15];%每张发票金额
+amount_baoxiao = 7350;%要报金额
+invoice = [586,631,786,595,710,1300,101,432,1300,500,409,196,305,182];%每张发票金额
 dim = length(invoice(:));
 lu=repmat([0;1],[1,dim]);
 
@@ -12,7 +12,21 @@ F = 0.9;
 CR = 0.9;
 popsize = 30;
 
-p = repmat(lu(1, :), popsize, 1) + rand(popsize, dim) .* (repmat(lu(2, :) - lu(1, :), popsize, 1));
+invoice_order = randperm(dim);
+p = repmat(lu(1, :), popsize, 1) + ( rand(popsize, dim)*0.5) .* (repmat(lu(2, :) - lu(1, :), popsize, 1));
+for ind = 1 : popsize
+    for i = 1 : dim
+        ind_select_prob = rand(1,dim)*0.5 + 0.5;
+        idx = invoice_order(1:i);
+        invoice_try = invoice(:,idx);
+        invoice_sum = sum(invoice_try);
+        if invoice_sum >= amount_baoxiao
+            p(ind,idx) = ind_select_prob(1,idx);
+            break;
+        end
+    end
+end
+%p = repmat(lu(1, :), popsize, 1) + rand(popsize, dim) .* (repmat(lu(2, :) - lu(1, :), popsize, 1));
 FES = popsize;
 
 % Evaluate the objective function values
@@ -45,3 +59,4 @@ res = invoice(best_ind);
 fprintf('报销金额 %d\n', amount_baoxiao);
 fprintf('小仙女手里的发票 %s\n',mat2str(invoice));
 fprintf('小仙女要报的发票 %s\n',mat2str(res));
+fprintf('小仙女要报的发票总金额 %s\n',mat2str(sum(res)));
